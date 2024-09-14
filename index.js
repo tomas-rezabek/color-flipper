db437316df34567f5be15bbc1ab50423703e75ea
tomas-rezabek <git@tomasrezabek.cz>
2024-09-14 11:15:05 +0200

    added random color

--- index.js
+++
@@ -1,8 +0,0 @@
-const body = document.getElementsByTagName("body")[0];
-
-function setColor(color) {
-  body.style.backgroundColor = color;
-}
-
-
-
---
+++ js/index.js
@@ -0,0 +1,15 @@
+const body = document.getElementsByTagName("body")[0];
+
+function setColor(color) {
+  body.style.backgroundColor = color;
+}
+
+function randomColor() {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+
+  const color = `rgb(${red}, ${green}, ${blue})`
+  body.style.backgroundColor = color;
+}
+
